6486038a05b29bf7fb2110c2b4f40b1187c94537
AyStarc <21ucc030@lnmiit.ac.in>
2024-02-01 00:09:19 +0530

    Basic Layout of homescreen

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:stacked_listview/stacked_listview.dart';
+import 'package:viva_app/Widgets/homepage_tile.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +12,48 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Home");
+    return MaterialApp(
+      home: SafeArea(
+        child: Column(children: [
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            height: 200,
+            child: ListView(
+              // This next line does the trick.
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                Container(
+                  width: 160,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: 160,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: 160,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  width: 160,
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.sizeOf(context).height * 0.5,
+            child: StackedListView(
+                itemCount: 50,
+                builder: (BuildContext context, int index) {
+                  return Card(
+                    child: Text("abcd"),
+                  );
+                },
+                itemExtent: MediaQuery.sizeOf(context).height * 0.3),
+          )
+        ]),
+      ),
+    );
   }
 }
